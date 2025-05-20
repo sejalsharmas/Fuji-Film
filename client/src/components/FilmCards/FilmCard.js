fef1b1077bfa42ba4a14e0806be6e42e495cfb0d
sejslsharmas <sejalsharmaany@gmail.com>
2025-05-20 15:24:40 +0530

    delete functionallity changed

--- client/src/components/FilmCards/FilmCard.js
+++ client/src/components/FilmCards/FilmCard.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './FilmCard.css'
 import Chair from './../../assets/director-chair.png'
 import Year from './../../assets/calendar.png'
@@ -7,74 +7,78 @@ import hindi from './../../assets/language.png'
 import category from './../../assets/cinema.png'
 import Button from '../Button/Button'
 import axios from 'axios'
-import toast, {Toaster} from 'react-hot-toast'
-import {useNavigate} from 'react-router-dom'
+import toast, { Toaster } from 'react-hot-toast'
+import { useNavigate } from 'react-router-dom'
 
 function FilmCard({
-    title,
-    shortDescription,
-    genre,
-    director,
-    year,
-    language,
-    poster,
-    rating,
-    _id
-}) 
-{
+  title,
+  shortDescription,
+  genre,
+  director,
+  year,
+  language,
+  poster,
+  rating,
+  _id
+}) {
   const navigate = useNavigate()
-  const deleteFilm = async() =>{
-      const response = await axios.delete(`${process.env.REACT_APP_API_URL}/films/${_id}`)
+  const [visible, setVisible] = useState(true) 
 
+  const deleteFilm = async () => {
+    try {
+      const response = await axios.delete(`${process.env.REACT_APP_API_URL}/films/${_id}`)
       toast.success(response.data.message)
-      window.location.reload()
+      setVisible(false) 
+    } catch (error) {
+      console.error(error)
+      toast.error('Failed to delete film')
+    }
   }
+
+  if (!visible) return null 
+
   return (
     <div className='filmcard'>
       <div>
-         <img src={poster} alt={title} className='film-card-poster'></img>
+        <img src={poster} alt={title} className='film-card-poster' />
       </div>
       <div>
-          <h1 className='film-card-info'>{title}</h1>
-          <div className='film-card-info-details'>
-            <span className='director'>
-              <img src={Chair} className='film-info-icon' alt='img'></img>
-              <b>{director}</b> 
-            </span>
-            <span className='year'>
-              <img src={Year} className='film-info-icon' alt='img'></img>
-               <b>{year}</b> 
-            </span>
-            <span className='Rating' >
-              <img src={Rating} className='stars' alt='img'></img>
-              :<b>{rating}</b>
-            </span>
-          </div>
-          <p className='describe' >{shortDescription}</p>
-          <div className='film-details'>
-            <span className='hindi'>
-              <img src={category} className='film-info-icon' alt='img'></img>
-              {genre}</span>
-            <span className='category'>
-              <img src={hindi} className='film-info-icon' alt='img'></img>
-              {language}</span>
-          </div>
+        <h1 className='film-card-info'>{title}</h1>
+        <div className='film-card-info-details'>
+          <span className='director'>
+            <img src={Chair} className='film-info-icon' alt='img' />
+            <b>{director}</b>
+          </span>
+          <span className='year'>
+            <img src={Year} className='film-info-icon' alt='img' />
+            <b>{year}</b>
+          </span>
+          <span className='Rating'>
+            <img src={Rating} className='stars' alt='img' />:<b>{rating}</b>
+          </span>
+        </div>
+        <p className='describe'>{shortDescription}</p>
+        <div className='film-details'>
+          <span className='hindi'>
+            <img src={category} className='film-info-icon' alt='img' />
+            {genre}
+          </span>
+          <span className='category'>
+            <img src={hindi} className='film-info-icon' alt='img' />
+            {language}
+          </span>
+        </div>
       </div>
       <div className='btn-row'>
-         <div className='edit-btn'>
-          <Button title= "Edit"  
-          onClick={() => {navigate(`/film/edit/${_id}`)}}/>
-         </div>
-         <div className='delete-btn'>
-       <Button
-        title="Delete"
-        onClick={() => deleteFilm ()}
-        />
+        <div className='edit-btn'>
+          <Button title="Edit" onClick={() => navigate(`/film/edit/${_id}`)} />
         </div>
+        <div className='delete-btn'>
+          <Button title="Delete" onClick={deleteFilm} />
         </div>
-       <Toaster/>
-      
-   </div>
+      </div>
+      <Toaster />
+    </div>
   )
 }
 
